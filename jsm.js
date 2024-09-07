d35c4e106f433ee2222978b46f5b8e3aa592bc5a
J M <jm@Js-MacBook-Air.local>
2024-09-07 15:39:15 +0300

    Javascript quiz assignment

---
+++ jsm.js
@@ -0,0 +1,34 @@
+const quizQuestions = [
+    {
+        question: "What is the capital of France?",
+        answers: "A: Paris, B: London, C: Berlin, D: Washington DC",
+        correctAnswer: "A"
+    },
+    {
+        question: "Which planet is known as the Red Planet?",
+        answers: "A: Earth, B: Mars, C: Jupiter, D: Venus",
+        correctAnswer: "B"
+    },
+    {
+        question: "What is the largest ocean on Earth?",
+        answers: "A: Atlantic Ocean, B: Indian Ocean, C: Pacific Ocean, D: Arctic Ocean",
+        correctAnswer: "C"
+    }
+];
+
+
+let score = 0;
+
+for (let i = 0; i < quizQuestions.length; i++) {
+    console.log(currentQuestion = quizQuestions[i]);
+
+    const userAnswer = prompt(currentQuestion.question + " " + currentQuestion.answers);
+    if (userAnswer == currentQuestion.correctAnswer) {
+        alert("Correct!");
+        score++; 
+    } else {
+        alert("Wrong! The correct answer was " + currentQuestion.correctAnswer);
+    }
+}
+
+alert("You got" + " " + score + " " + "out of" + " " + quizQuestions.length + " " + "correct!");
